d0ec9d621ce49e8ef83e96cdce87b899389a3f68
juan <juanfelipeperdomo41@gmail.com>
2025-08-07 23:40:39 -0500

    register y login estilisados y sus label y input hechos falta backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
+         
         </Routes>
 
         <ul className="circles">
--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -1,10 +1,53 @@
+import { Link } from "react-router-dom";
+import React from 'react';  
+import '../style/register.css';
+
 function Register() {
   return (
-    <div>
-      <h2>Página de Registro</h2>
-      {/* Aquí tu formulario */}
-    </div>
-  );
+    <>
+      
+    <form >
+        <h2>Registrate</h2>
+
+        <label>Nombre completo
+        <input
+            type="text"
+            placeholder="Nombre completo"
+            required
+        />
+        </label>
+
+        <label>Correo electrónico
+        <input
+            type="email"
+            placeholder="Correo electrónico"
+            required
+        />
+        </label>
+
+        <label>Contraseña
+        <input
+            type="password"
+            placeholder="Contraseña"
+            
+            required
+        />
+        </label>
+        <label>Telefono
+        <input
+            type="number"
+            placeholder="Telefono"
+            
+            required
+        />
+        </label>
+
+        <button className="enviar" type="submit">Registrate</button>
+        <Link to="../" className="login-btn"> ¿Ya tienes cuenta?</Link>
+    </form>
+   
+    </>
+);
 }
 
 export default Register; // 👈 Esto es lo que falta o está mal
