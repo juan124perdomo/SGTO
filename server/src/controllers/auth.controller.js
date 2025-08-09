b846ea3e3690bbb0ae6f9aecb39f5cdf849db84f
juan <juanfelipeperdomo41@gmail.com>
2025-08-09 11:12:50 -0500

    logout y haciendo rutas protegidas

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,9 +1,9 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken" ;
+import createAccesToken from "../libs/jwt.js" ;
 
 
-
+//Crea el usuario
 
 export const register =  async (req, res) => {
   const {email,password,username,telefono} = req.body
@@ -20,29 +20,65 @@ export const register =  async (req, res) => {
     
   });
 
-  jwt.sign({
-    id:userSaved._id,
-    
-  },"secret123",{expiresIn:"1d"},(err,token)=>{
-    if (err) console.log(err);
-      res.json({token});
-  });
-
-
-
-  const userSaved = await newUser.save();
-  res.json({
+    const userSaved = await newUser.save();
+    const token = await createAccesToken({id:userSaved._id}); 
+    res.cookie("token",token)
+   res.json({
     id: userSaved._id,
     username: userSaved.username,
     email: userSaved.email,
     telefono: userSaved.telefono,
-  })
-  } catch (error) {
+  });
+} catch (error) {
     console.log(error);
   }
 
   
 };
 
+//inicia seccion con el usuario
 
-export const login = (req,res)=>{ res.send("login")};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Validar si faltan datos
+    if (!email || !password) {
+      return res.status(400).json({ message: "Faltan datos" });
+    }
+
+    const userFound = await User.findOne({ email });
+    if (!userFound) {
+      return res.status(400).json({ message: "usuario no encontrado" });
+    }
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "contraseña incorrecta" });
+    }
+
+    const token = await createAccesToken({ id: userFound._id });
+    res.cookie("token", token);
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      telefono: userFound.telefono,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+export const logout = async (req, res) => {
+  res.cookie("token","",{
+    expires: new Date(0)
+  });
+  return res.sendStatus(200);
+}
+
+export const profile = async (req, res) => {
+  res.json({ message: "Perfil de usuario" });
+};
+
---
+++ server/src/middleware/validateToken.js
@@ -0,0 +1,8 @@
+
+
+export const authRequired = (req, res, next) => {
+    console.log(req.headers);
+
+
+next()
+}
