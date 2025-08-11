3772b8080c9fc5ffd74d3eff7f3c380d358dc079
juan <juanfelipeperdomo41@gmail.com>
2025-08-10 21:15:25 -0500

    por problemas   de ordenamiento toco borrar lo que estaba hecho en el frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,23 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-import Login from "./components/login";
-import Register from "./components/register";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 function App() {
   return (
-    
+    <>
     <Router>
-      <div className="login_register">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-         
-        </Routes>
-
-        <ul className="circles">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
+      <Routes>
+        <Route path="/" element={<h1>Inicio</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/ordenes" element={<Register />} />
+        <Route path="/add-orden" element={<Register />} />
+        <Route path="/ordenes/:id" element={<Register />} />
+        <Route path="/profile" element={<Register />} />
+      </Routes>
     </Router>
+    </>
   );
 }
 
